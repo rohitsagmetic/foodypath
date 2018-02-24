33050d1416e78be7824f5ec248e40dbbcd40113e
adictonator <adityabhaskarsharma@gmail.com>
2018-02-25 00:08:41 +0530

    Initial Commit & Cleaning Up
    Initial commit yo!

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+    render() {
+        return (
+            <header style={{'text-align': 'center'}}>
+                <h1>Foody Path</h1>
+                <p>Will be up and feeding really soon! Hopefully!</p>
+            </header>
+        )
+    }
+}
+
+export default App;
